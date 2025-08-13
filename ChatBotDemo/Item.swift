91046a9af07f45968e03adf293dac538b21ca36c
haisombo <haisombo20@gmail.com>
2025-08-13 13:42:10 +0700

    Initial Commit

---
+++ ChatBotDemo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ChatBotDemo
+//
+//  Created by Sombo Mobile R&D on 13/8/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ChatBotDemoTests/ChatBotDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatBotDemoTests.swift
+//  ChatBotDemoTests
+//
+//  Created by Sombo Mobile R&D on 13/8/25.
+//
+
+import Testing
+
+struct ChatBotDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
